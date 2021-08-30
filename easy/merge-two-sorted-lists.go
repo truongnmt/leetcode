88d6695566a75d787db98d70b5895a841a354827
truongnmt <nguyen.manh.truong@moneyforward.co.jp>
2021-08-31 07:48:15 +0900

    merge-two-sorted-lists dummy pointer

--- easy/merge-two-sorted-lists.go
+++ easy/merge-two-sorted-lists.go
@@ -63,5 +63,42 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
-
+    if l1 == nil {
+        return l2
+    }
+    if l2 == nil {
+        return l1
+    }
+    
+    head := l1
+    if l2.Val < l1.Val {
+        head = l2
+        l2 = l2.Next
+    } else {
+        l1 = l1.Next
+    }
+    
+    tmp := head
+    for l1 != nil || l2 != nil {
+        if l1 == nil {
+            tmp.Next = l2
+            break;
+        }
+        if l2 == nil {
+            tmp.Next = l1
+            break;
+        }
+        
+        if l1.Val < l2.Val {
+            tmp.Next = l1
+            tmp = l1
+            l1 = l1.Next
+        } else {
+            tmp.Next = l2
+            tmp = l2
+            l2 = l2.Next
+        }
+    }
+    
+    return head
 }
