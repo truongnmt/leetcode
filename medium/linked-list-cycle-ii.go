b0c2b23c2f0510a2b6bf29afc08e726cb6159c5c
truongnmt <nguyen.manh.truong@moneyforward.co.jp>
2021-08-31 09:43:48 +0900

    linked-list-cycle-ii with tortoise and hare algorithm

--- medium/linked-list-cycle-ii.go
+++ medium/linked-list-cycle-ii.go
@@ -20,3 +20,30 @@
     }
     return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return nil
+    }
+    
+    slow := head
+    fast := head
+    var meetingPoint *ListNode
+    for fast.Next != nil && fast.Next.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if slow == fast {
+            meetingPoint = slow
+            break
+        }
+    }
+    
+    if meetingPoint == nil {
+        return nil
+    }
+    for head != meetingPoint {
+        head = head.Next
+        meetingPoint = meetingPoint.Next
+    }
+    return head
+}
