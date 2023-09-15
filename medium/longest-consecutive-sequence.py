79c114c934cf495346cf70c00a1c8db6cf6081dd
TruongNM <truongnmt.dev@gmail.com>
2023-09-16 00:00:09 +0700

    add

--- medium/longest-consecutive-sequence.py
+++ medium/longest-consecutive-sequence.py
@@ -6,6 +6,13 @@
 # for num in range(min..max):
 #     if num and num+1 exists in set
 #         count++
+# ^ this will TLE
+
+# set()
+# for num in nums_set
+# if num-1 not in set, count =1
+#    while num+count in nums_set, 
+#        increment count and check max_count
 
 
 class Solution:
@@ -13,19 +20,13 @@ class Solution:
         if len(nums) == 0: return 0
 
         nums_set = set()
-        min_n = max_n = nums[0]
-
         for num in nums:
             nums_set.add(num)
-            min_n = min(min_n, num)
-            max_n = max(max_n, num)
 
         max_count = count = 1
         for num in nums_set:
-            
             if num-1 not in nums_set:
                 count = 1
-
                 while num+count in nums_set:
                     count += 1
                 max_count = max(max_count, count)
