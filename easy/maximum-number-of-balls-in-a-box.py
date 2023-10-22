6bc619b871b04dba408e4012219486b2aedcb229
TruongNM <truongnmt.dev@gmail.com>
2023-10-22 12:13:55 +0900

    add

--- easy/maximum-number-of-balls-in-a-box.py
+++ easy/maximum-number-of-balls-in-a-box.py
@@ -3,24 +3,33 @@
 # count the bucket size
 
 class Solution:
+    # def countBalls(self, lowLimit: int, highLimit: int) -> int:
+    #     # create buckets
+    #     boxes = defaultdict(list)
+    #     maxBallCount = 0
+
+    #     # hash the ball
+    #     for i in range(lowLimit, highLimit+1):
+    #         hashResult = self.hashBall(i)
+    #         boxes[hashResult].append(i)
+    #         maxBallCount = max(maxBallCount, len(boxes[hashResult]))
+    #         # print(f"i={i} hashResult={hashResult} boxes[hashResult]={boxes[hashResult]} maxBallCount={maxBallCount}")
+
+    #     return maxBallCount
+
+    # def hashBall(self, num: int) -> int:
+    #     sum = 0
+    #     while num>0:
+    #         sum += num%10
+    #         num //= 10 # 321 / 10 = 32 rem 1; 32/10 = 3 rem 2; 3/10 = 0 rem 3
+
+    #     return sum
+
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
-        # create buckets
-        boxes = defaultdict(list)
-        maxBallCount = 0
+        freq = defaultdict(int)
 
-        # hash the ball
-        for i in range(lowLimit, highLimit+1):
-            hashResult = self.hashBall(i)
-            boxes[hashResult].append(i)
-            maxBallCount = max(maxBallCount, len(boxes[hashResult]))
-            # print(f"i={i} hashResult={hashResult} boxes[hashResult]={boxes[hashResult]} maxBallCount={maxBallCount}")
+        for num in range(lowLimit, highLimit+1):
+            sumBall = sum(int(digit) for digit in str(num))
+            freq[sumBall] += 1
 
-        return maxBallCount
-
-    def hashBall(self, num: int) -> int:
-        sum = 0
-        while num>0:
-            sum += num%10
-            num //= 10 # 321 / 10 = 32 rem 1; 32/10 = 3 rem 2; 3/10 = 0 rem 3
-
-        return sum
+        return max(freq.values())
